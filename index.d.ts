0a43ccc0c6af578342b2687f6aa2bf7e7ad132aa
HanRui <597589373@qq.com>
2020-05-14 18:32:24 +0800

    增加库功能

---
+++ index.d.ts
@@ -0,0 +1,14 @@
+interface DownProps {
+    reqUrl: strubg,
+    downFileName: string
+}
+
+declare const res: any
+
+declare const hideLink: any
+
+declare const objectUrl: any
+
+declare const streamDownload: DownProps
+
+export default streamDownload
