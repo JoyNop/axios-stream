bedfb4cb1c66684f7498e2d1b17491dc46315d5c
HanRui <597589373@qq.com>
2020-05-15 09:58:35 +0800

    1

--- index.d.ts
+++ index.d.ts
@@ -8,10 +8,10 @@
  * @param reqUrl 
  * @param downFileName 
  */
-// declare function streamDownload(reqUrl : string, downFileName : string): void;
+declare function streamDownload(reqUrl : string, downFileName : string): void;
 
 
 
-declare const streamDownload: (reqUrl: string, downloadFileName: string) => void;
+// declare const streamDownload: (reqUrl: string, downloadFileName: string) => void;
 
 export default streamDownload;
