4e40d168646ae8b5ce479ad5b32d38a5fe290337
HanRui <597589373@qq.com>
2020-05-15 08:46:19 +0800

    1

--- index.js
+++ index.js
@@ -6,29 +6,34 @@ const axios = require('axios');
  * @description 数据流文件下载
  * @param {string} reqUrl 请求的API
  * @param {string} downFileName  请求的下载名称，必须指定文件名和扩展名
+ * 
  */
-const streamDownload = async (reqUrl, downFileName) => {
-  try {
-    const res = await axios.get(reqUrl, {
-      responseType: "blob",
-    });
-    const blob = new Blob([res.data], {
-      type: "application/octet-stream",
-    });
-    const hideLink = document.createElement("a");
-    hideLink.style.display = "none";
-    const objectUrl = window.URL.createObjectURL(blob);
-    hideLink.href = objectUrl;
-    hideLink.download = downFileName;
-    document.body.appendChild(hideLink);
-    hideLink.click();
-    document.body.removeChild(hideLink);
-  } catch (error) {
-    throw error
-  }
-
-};
 
 
-module.exports = streamDownload
+
+  export const streamDownload = async (reqUrl, downFileName) => {
+    try {
+      const res = await axios.get(reqUrl, {
+        responseType: "blob",
+      });
+      const blob = new Blob([res.data], {
+        type: "application/octet-stream",
+      });
+      const hideLink = document.createElement("a");
+      hideLink.style.display = "none";
+      const objectUrl = window.URL.createObjectURL(blob);
+      hideLink.href = objectUrl;
+      hideLink.download = downFileName;
+      document.body.appendChild(hideLink);
+      hideLink.click();
+      document.body.removeChild(hideLink);
+    } catch (error) {
+      throw error
+    }
+
+  };
+
+
+module.exports=streamDownload
+
 
