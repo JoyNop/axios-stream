c0a0e78f9422ba460bde7d1619f5a7084dc90398
HanRui <597589373@qq.com>
2020-05-15 09:07:08 +0800

    1

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+// Type definitions for index.js
+// Project: [LIBRARY_URL_HERE] 
+// Definitions by: [YOUR_NAME_HERE] <[YOUR_URL_HERE]> 
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+/**
+ * 
+ * @param reqUrl 
+ * @param downFileName 
+ */
+declare function streamDownload(reqUrl : string, downFileName : string): void;
